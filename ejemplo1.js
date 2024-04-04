a39be4d31b3babce78f20befe8ea7b48736dd0d7
Santiagor002 <santiagobravo383@gmail.com>
2024-04-04 17:53:17 -0300

    mi primer commit

---
+++ ejemplo1.js
@@ -0,0 +1,21 @@
+/* console.log("muy buenas gente")
+ */
+
+var c="Milanesa."
+var a="Guiso."
+var m="Mondongo."
+/* console.log("papasfritas")*/
+
+var menu= "Menu \n- " + c + "\n- " + a + "\n- " + m
+console.log(menu) 
+
+
+let entero = 3 /* esto es una variable de tipo entero */
+let nombreApellido = "Juan Perez" /* esto es una variable de tipo string */
+let flotante = 1.5 /* esto es una variable de tipo flotante */
+let booleana = true /* esto es una variable de tipo booleana (true o flase) */
+
+console.log(entero)
+console.log(nombreApellido)
+console.log(flotante)
+console.log(booleana)
