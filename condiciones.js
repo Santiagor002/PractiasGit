593ed66ffa0a555b452a7205ac651074c4ec5514
Santiagor002 <santiagobravo383@gmail.com>
2024-04-04 18:09:28 -0300

    agregue una palabra

---
+++ condiciones.js
@@ -0,0 +1,37 @@
+var p = true
+var q = true
+
+/* este es un if condicinal con NEGACION /
+if(!p){
+    console.log("true")
+}else{
+    console.log("false")
+}
+
+/ este es un if condicinal con AND /
+if(p && q){
+    console.log("true")
+}else{
+    console.log("false")
+}
+
+/ este es un if condicinal con OR /
+if(p || q){
+    console.log("true")
+}else{
+    console.log("false")
+}
+
+/ este es un if condicinal con NAND /
+if(!p && q){
+    console.log("true")
+}else{
+    console.log("false")
+}
+
+/ este es un if condicinal con NOR */
+if(!p || q){
+    console.log("true")
+}else{
+    console.log("false")
+}
